967ef2b98c45c76af025821be0ccd66bd38c9171
ShevtsovAlexander <alexander.shevtsovvv.com>
2023-04-27 17:43:30 +0300

    captcha

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -7,12 +7,15 @@ import { login } from '../../redux/auth-reducer';
 import style from './../common/FormsControls/FormsControls.module.css';
 import { Redirect } from 'react-router-dom';
 
-const LoginForm = ({ handleSubmit, error }) => {
+const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
   return (
     <form onSubmit={handleSubmit}>
       {createField('Email', 'email', [required], Input)}
       {createField('Password', 'password', [required], Input, { type: 'password' })}
       {createField(null, 'rememberMe', [], Input, { type: 'checkbox' }, 'remember me')}
+
+      {captchaUrl && <img src={captchaUrl} />}
+      {captchaUrl && createField('Symbols from image', 'captcha', [required], Input, {})}
 
       {error && <div className={style.formSummaryError}>{error}</div>}
       <div>
@@ -26,7 +29,7 @@ const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
 
 const Login = ({ login, isAuth }) => {
   const onSubmit = (formData) => {
-    login(formData.email, formData.password, formData.rememberMe);
+    login(formData.email, formData.password, formData.rememberMe, formData.captchaUrl);
   };
 
   if (isAuth) {
@@ -35,11 +38,12 @@ const Login = ({ login, isAuth }) => {
   return (
     <div>
       <h1>Login</h1>
-      <LoginReduxForm onSubmit={onSubmit} />
+      <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
     </div>
   );
 };
 const mapStateToProps = (state) => ({
   isAuth: state.auth.isAuth,
+  captchaUrl: state.auth.captchaUrl,
 });
 export default connect(mapStateToProps, { login })(Login);
